e376ca5ae41e02823fed9dda3cf6c7d85b596468
eliza <elizase405@gmail.com>
2023-09-13 03:16:11 -0700

    Fixed errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copied_list = my_list
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return copied_list
 
     new_list = [my_list[i] for i in range(len(my_list))]
