046b2144629db539dda6cb626a5bebd4c4c32abb
eliza <elizase405@gmail.com>
2023-09-25 14:07:23 -0700

    Fix error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-
-    _argv = sys.argv
+    n = len(sys.argv)
     sum = 0
-    argv_len = len(_argv)
-
-    if argv_len == 1:
-        print("{:d}".format(sum))
-    else:
-        for i in range(1, argv_len):
-            sum += int(_argv[i])
-            print("{:d}".format(sum))
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+    print(sum)
