67f8fd5cdfb4cc31feab459db3c96e1e18a2b288
eliza <elizase405@gmail.com>
2023-09-26 14:48:38 -0700

    Finished project 6

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,19 @@
+class Square:
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** self.__size)
+
+    @property
+    def size(self):
+        return __size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+# Learning about Classes in python
