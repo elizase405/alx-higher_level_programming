99f77d255870169e2bf660f014701788fe7a2bf3
eliza <elizase405@gmail.com>
2023-10-02 14:46:08 -0700

    fix comment error

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """Defining a rectanlge class"""
+
 
 class Rectangle:
     pass
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """Defining a rectanlge class"""
+
 
 class Rectangle:
     def __init__(self, width=0, height=0):
