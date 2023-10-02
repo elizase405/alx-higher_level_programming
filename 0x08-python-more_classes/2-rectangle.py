6974a82de974b057cb90c40711453c2f18a49163
eliza <elizase405@gmail.com>
2023-10-02 14:37:48 -0700

    Did task 0 - 3

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if type(value) != int:
+            print("width must be an integer")
+            raise TypeError
+        if value > 0:
+            print("width must be >= 0")
+            raise ValueError
+        self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if type(value) != int:
+            print("height must be an integer")
+            raise TypeError
+        if value > 0:
+            print("height must be >= 0")
+            raise ValueError
+        self.__height = value
+
+    def area(self):
+        return height * width
+
+    def perimeter(self):
+        return (2 * height) + (2 * width)
