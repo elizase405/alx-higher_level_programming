0ab215288b54367ae0fc394b4bf1bbb4ee6090e7
eliza <elizase405@gmail.com>
2023-10-16 11:14:15 -0700

    Did task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module contains 1 class
+that inherits from the rectangle class:
+    s = Square()
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is the Square class.
+
+    Args: Rectangle
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes instance """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ returns output when str() or print() is used """
+
+        cls = "[Square] "
+        idd = "({}) ".format(self.id)
+        xy = "{}/{} - ".format(self.x, self.y)
+        wh = "{}/{}".format(self.width, self.height)
+
+        return cls + idd + xy + wh
