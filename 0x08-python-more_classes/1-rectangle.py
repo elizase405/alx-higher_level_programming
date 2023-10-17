e226c0a4874c2316e173409e79cf24e735c033e1
eliza <elizase405@gmail.com>
2023-10-17 05:08:03 -0700

    Documented module again

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-
 This module is composed by a class that defines a Rectangle
-
-
 """
 
 
@@ -12,17 +9,20 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ initializes instance """
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """Returns object width"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
         """Sets object width"""
+
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value > 0:
@@ -32,11 +32,13 @@ class Rectangle:
     @property
     def height(self):
         """Gets object height"""
+
         return self.__height
 
     @height.setter
     def height(self, value):
         """Sets object height"""
+
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value > 0:
