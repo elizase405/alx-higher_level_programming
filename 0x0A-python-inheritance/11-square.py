a2e45b3404b6814b172aa7658e914f646317c70a
eliza <elizase405@gmail.com>
2023-10-29 11:41:33 -0700

    Add module documentation for task 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-
 """
 Module containing 1 class:
     square
 """
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
