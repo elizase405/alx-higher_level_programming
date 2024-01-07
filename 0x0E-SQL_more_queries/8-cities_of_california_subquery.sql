730d71048630bb4326d94613ee9399a10f1bc428
Elizabeth <elizase405@gmail.com>
2024-01-06 21:54:04 -0800

    fix: Change == to LIKE for comparison

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name FROM cities
-WHERE (SELECT id FROM states WHERE name == "California") == state_id
+SELECT id, name from cities
+WHERE state_id LIKE (SELECT id FROM states WHERE name LIKE "California")
 ORDER BY id;
