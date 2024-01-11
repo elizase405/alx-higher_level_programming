67b2c0aefef32ead054c05a764c6f259eb01d845
Elizabeth <elizase405@gmail.com>
2024-01-11 13:23:02 -0800

    fix: fix error

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 
-SELECT tvg.name, COUNT(tvs.title) AS number_of_shows
+SELECT tvg.name, COUNT(tvg.name) AS number_of_shows
 FROM tv_genres AS tvg
 INNER JOIN tv_show_genres AS tvsg
 ON tvg.id LIKE tvsg.genre_id
