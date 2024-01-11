b5f6b7f5411a03e34e57a7886b5777f1cc51ab33
Elizabeth <elizase405@gmail.com>
2024-01-11 13:38:39 -0800

    refactor: trying a new style

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,5 @@ SELECT tvg.name AS genre, COUNT(tvg.name) AS number_of_shows
 FROM tv_genres AS tvg
 INNER JOIN tv_show_genres AS tvsg
 ON tvg.id LIKE tvsg.genre_id
-INNER JOIN tv_shows AS tvs
-ON tvsg.show_id LIKE tvs.id
 GROUP BY tvg.name
 ORDER BY number_of_shows DESC;
