8515e2cacc0b4efec70c3a022f6786608ac3e237
Elizabeth <elizase405@gmail.com>
2024-01-11 13:46:10 -0800

    feat: task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+
+SELECT tvg.name
+FROM tv_genres AS tvg
+INNER JOIN tv_show_genres AS tvsg
+ON tvsg.genre_id LIKE tvg.id
+INNER JOIN tv_shows as tvs
+ON tvsg.show_id LIKE tvs.id
+WHERE tvs.title LIKE "Dexter"
+ORDER BY tvg.name;
