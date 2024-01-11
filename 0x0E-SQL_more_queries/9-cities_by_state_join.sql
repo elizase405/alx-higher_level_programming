be5c3a3ccfb3b3b6c9eb45efeec4b6a2634ec87e
Elizabeth <elizase405@gmail.com>
2024-01-11 11:27:12 -0800

    fix: Fix incorrect output

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT id, c.name, s.name AS name
+SELECT c.id, c.name, s.name AS name
 FROM cities AS c
 INNER JOIN states AS s
-USING (id)
+ON c.state_id LIKE s.id
 ORDER BY c.id;
