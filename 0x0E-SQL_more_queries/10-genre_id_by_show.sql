86b343c286e05d83251066a4108b0960badf5512
Elizabeth <elizase405@gmail.com>
2024-01-11 12:36:07 -0800

    refactor: Use correct command

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 SELECT tvs.title, tvsg.genre_id
 FROM tv_shows AS tvs
 INNER JOIN tv_show_genres AS tvsg
-WHERE tvs.id LIKE tvsg.show_id
+ON tvs.id LIKE tvsg.show_id
 ORDER BY tvs.title, tvsg.genre_id;
