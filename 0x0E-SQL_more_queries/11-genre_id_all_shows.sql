f8790ca376749e2dae024c8f6bca4ebb39ba24fc
Elizabeth <elizase405@gmail.com>
2024-01-11 12:31:06 -0800

    feat: Task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- a script that lists all shows contained in the database hbtn_0d_tvshows. If a show doesn’t have a genre, display NULL
+
+SELECT tvs.title, tvsg.genre_id
+FROM tv_shows AS tvs
+LEFT JOIN tv_show_genres AS tvsg
+ON tvs.id LIKE tvsg.show_id
+ORDER BY tvs.title, tvsg.genre_id;
