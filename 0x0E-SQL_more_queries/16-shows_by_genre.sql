c34c46f114e81d412041dce3e09e0a4867bd3d86
Elizabeth <elizase405@gmail.com>
2024-01-16 10:22:11 -0800

    feat: task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+--  lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+
+SELECT tvs.title, tvg.name
+FROM tv_shows AS tvs
+LEFT JOIN tv_show_genres AS tvsg
+ON tvs.id LIKE tvsg.show_id
+LEFT JOIN tv_genres AS tvg
+ON tvg.id LIKE tvsg.genre_id
+ORDER BY tvs.title, tvg.name ASC;
