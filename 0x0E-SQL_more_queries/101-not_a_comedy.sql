ff5da6bdd6d09a3f36aba2dcf382b9d9cb2e0509
Elizabeth <elizase405@gmail.com>
2024-01-21 21:42:52 -0800

    task 18

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,18 @@
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+
+SELECT DISTINCT title
+FROM tv_shows AS tvs
+LEFT JOIN tv_show_genres AS tvsg
+ON show_id LIKE tvs.id
+LEFT JOIN tv_genres as tvg
+ON tvg.id LIKE genre_id
+WHERE title NOT IN (
+        SELECT title
+        FROM tv_shows AS tvs
+        INNER JOIN tv_show_genres AS tvsg
+        ON show_id LIKE tvs.id
+        INNER JOIN tv_genres as tvg
+        ON tvg.id LIKE genre_id
+        WHERE name LIKE "COMEDY"
+)
+ORDER BY title;
