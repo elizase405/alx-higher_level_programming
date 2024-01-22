2d54fd1b0c56335747b369132edfce9cb31609e1
Elizabeth <elizase405@gmail.com>
2024-01-21 21:43:52 -0800

    refactor: sql keywords in uppercase

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,14 +4,14 @@ SELECT DISTINCT title
 FROM tv_shows AS tvs
 LEFT JOIN tv_show_genres AS tvsg
 ON show_id LIKE tvs.id
-LEFT JOIN tv_genres as tvg
+LEFT JOIN tv_genres AS tvg
 ON tvg.id LIKE genre_id
 WHERE title NOT IN (
         SELECT title
         FROM tv_shows AS tvs
         INNER JOIN tv_show_genres AS tvsg
         ON show_id LIKE tvs.id
-        INNER JOIN tv_genres as tvg
+        INNER JOIN tv_genres AS tvg
         ON tvg.id LIKE genre_id
         WHERE name LIKE "COMEDY"
 )
