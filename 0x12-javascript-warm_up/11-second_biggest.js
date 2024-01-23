72dbdf2fe3c33056b6caf8ef8d2a04f933a89dba
Elizabeth <elizase405@gmail.com>
2024-01-23 05:14:04 -0800

    refactore: used functional programming

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
 
-const arr = process.argv;
-const maxArr = []; // stores all the biggest integer found and replaced
-let ind = -1; // stores the index of the biggest integer
-let maxVal = 0; // stores the biggest integer
+const arr = process.argv.slice(2);
 
-// we start i at 2 because that is where command line input start
-for (let i = 2; i < arr.length; i++) {
-  if (arr[i] > maxVal) {
-    ind++;
-    maxVal = arr[i];
-    maxArr.push(arr[i]);
-  }
-}
-
-if (maxArr.length === 0 || maxArr.length === 1) {
+if (arr.length <= 1) {
   console.log(0);
 } else {
-  console.log(maxArr[ind - 1]); // we want the second biggest integer
+  const max = Math.max(...arr);
+  const secondMax = Math.max(...arr.filter(x => x < max));
+  console.log(secondMax);
 }
