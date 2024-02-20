3f83d7f9bc8d380a251bcb80f5d21b86e26e98cd
Elizabeth <elizase405@gmail.com>
2024-02-19 22:38:03 -0800

    fix: fix indentation error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,4 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         data = res.read()
-    print(f"""Body response:
-    - type: {type(data)}
-    - content: {data}
-    - utf8 content: {data.decode("utf8")}""")
+    print(f"Body response:\n\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode('utf8')}")
