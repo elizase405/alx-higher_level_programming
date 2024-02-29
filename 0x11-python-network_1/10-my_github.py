7f04b18e6b2dbf4a7e4af50a6a1b266dad1820c2
Elizabeth <elizase405@gmail.com>
2024-02-29 04:51:22 -0800

    feat: task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Login to my github using github API and print my id"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+
+    res =  requests.get(url, auth=HTTPBasicAuth(username, pwd))
+    print(res.json().get("id"))
