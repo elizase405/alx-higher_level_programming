bdd0fcb837d98a1ec04250d1f4d21a0a5ec20523
Elizabeth <elizase405@gmail.com>
2024-04-14 06:21:30 -0700

    feat: task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie
+// ./100-starwars_characters.js 3
+
+const req = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+req(url, (err, res, data) => {
+  if (err) console.log(err);
+
+  data = JSON.parse(data);
+  data.characters.filter(link =>
+    req(link, (err, res, data) => {
+      if (err) console.log(err);
+
+      data = JSON.parse(data);
+      console.log(data.name);
+    })
+  );
+});
